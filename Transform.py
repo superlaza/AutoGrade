7c8ca71b3da393489a52b35dcd3ec844b3015866
birdman7260 <mbird7260@gmail.com>
2014-01-29 12:49:11 -0500

    Update Transform.py

--- Transform.py
+++ Transform.py
@@ -21,12 +21,12 @@ def transform(im, points):
     template = cv2.cvtColor(cv2.imread('answerTemplate.jpg'), cv2.COLOR_BGR2GRAY)
     blank = np.ones((2200, 2200), dtype = np.uint8) * 255
     blank[:, :1700] = template
-    #template = cv2.resize(blank, (500,500))
-    template = cv2.resize(blank, (640, 480))
+    template = cv2.resize(blank, (500,500))
+    #template = cv2.resize(blank, (640, 480))
 
+    registered = cv2.cvtColor(registered, cv2.COLOR_BGR2GRAY) 
     print template.shape
-    print registered.shape
-    registered = cv2.cvtColor(registered, cv2.COLOR_BGR2GRAY)    
+    print registered.shape   
     blend = cv2.addWeighted(registered, .7, template, .3, 0)
 
     return registered, blend
