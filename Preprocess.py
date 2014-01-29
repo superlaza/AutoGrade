d9956470425260fbdf4782a1a1f2796529d91d8b
birdman7260 <mbird7260@gmail.com>
2014-01-29 12:30:20 -0500

    Create Preprocess.py

---
+++ Preprocess.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+def drawSquare(im, ((xo, yo), (x, y))):
+    cv2.line(im, (xo,yo), (x,yo), (0,255,0))
+    cv2.line(im, (x,yo), (x,y), (0,255,0))
+    cv2.line(im, (x,y), (xo,y), (0,255,0))
+    cv2.line(im, (xo,y), (xo,yo), (0,255,0))
+
+def region(im, big, small):
+    im[:big[0][0],:] = 0
+    im[:,:big[0][1]] = 0
+    im[big[1][0]:im.shape[1],:] = 0
+    im[:,big[1][1]:im.shape[0]] = 0
+    im[small[0][0]:small[1][0],small[0][1]:small[1][1]] = 0
+
+def preprocess(im, win, sbig, ssmall):
+    im = np.array(im[:,:im.shape[0],:])
+    im = cv2.resize(im, win)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    
+    edges = cv2.Canny(gray, 100, 240)
+
+    drawSquare(im, sbig)
+    drawSquare(im, ssmall)
+    
+    region(edges, sbig, ssmall)
+
+    return edges, im
