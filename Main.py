06b758f4edd7502ead76975706600d6916f1ece8
David Perlaza <superlaza7@gmail.com>
2014-02-08 00:37:17 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,118 +3,98 @@ import numpy as np
 from Preprocess import preprocess
 from FindPoints import findPoints
 from Transform import transform
-from Grade import grade,drawAnswerCoords
+from Grade import grade,ndrawAnswerCoords
 import Errors
 
 camera_port = 0
 ramp_frames = 4
-win = (500,500)
-sbig = ((80,80), (420,420))
-ssmall = ((150,150), (350,350))
 
 #for debugging
-'''opts: output, canny, blend'''
+'''opts: output, preprocessed, blend'''
 view = "output"
 suppress = True
-
-def get_image(camera):
-    retval, im = camera.read()
-    return im
 
 #to draw image with aligned circles to test alignment of regged image
 
 def main():
-
-    #for criterion
-    #minval can be any exorbitantly large value
-    minval = 5000000
+    #for criterion, minval can be any exorbitantly large value
+    minval = 500000000
     quota = 0
-
     
     camera = cv2.VideoCapture(camera_port)
 
     for i in xrange(ramp_frames):
-        temp = get_image(camera)
+        retval, temp = camera.read()
 
     while True:
-        if cv2.cv.WaitKey(10) == 27:
-            #if escaped, last ditch grade
-            grade(registered)
-            break
-        im = get_image(camera)
-
+        #The function waitKey waits for a key event infinitely (when delay<=0 )
+        #or for delay milliseconds, when it is positive. It returns the code of
+        #the pressed key or -1 if no key was pressed before the specified time
+        #had elapsed. Escape code is 27
+        wk = cv2.cv.WaitKey(10)
+        
+        retval, im = camera.read()
+        
         try:
-            edges, im = preprocess(im, win, sbig, ssmall)
+            edges, im = preprocess(im)
+
+            if view == "processed":
+                cv2.imshow('edges', edges)
+            if view == "output" or view =="region" or view == "blend":
+                cv2.imshow("image", im)
+            
+            intersections = findPoints(edges)
+            
+        except Errors.ImproperIntersectionsError as e:
+            if not suppress:
+                print e
+            continue
         except:
             if not suppress:
                 print "something bad is happening at preprocess"
             continue
 
-        if view == "canny":
-            cv2.imshow('edges', edges)
-
-        if view == "output" or view =="region" or view == "blend":
-            cv2.imshow("image", im)
-            cv2.imshow("edges", edges)
-
-        try:
-            intersections, im = findPoints(edges, im)
-        except Errors.NotEnoughCannyEdgesError as e:
-            if not suppress:
-                print e
-            continue
-        except Errors.NotEnoughIntersectionsError as e:
-            if not suppress:
-                print e
-            continue
-        except Errors.ImproperIntersectionsError as e:
-            if not suppress:
-                print e
-            continue
-
+        #if no points found, this code isn't seen
         #show computed intersection points
-        try:
-            for point in intersections:
-                cv2.circle(im, (int(round(point[0])), int(round(point[1]))),
-                           6, (0,0,255), -1)
-        except OverflowError as e:
-            if not suppress:
-                print e
-            continue
-
+        for point in intersections:
+            cv2.circle(im, (int(round(point[0])), int(round(point[1]))),
+                       6, (0,0,255), -1)
+        
         if view == "output":
             cv2.imshow("drawn intersections", im)
         
+
         try:
             registered = transform(im, intersections)
         except Errors.NotEnoughPointsToTransformError as e:
             if not suppress:
                 print e
             continue
+        
 
         '''==========BLENDING AND REGISTRATION CRITERION=========='''
                     #-----------------------------
                     #This is for visual comparison
                     #-----------------------------
         #template to show difference between it and the registered image
-        template_visual = cv2.cvtColor(cv2.imread('answerTemplate.jpg'), cv2.COLOR_BGR2GRAY)
+        template_visual = cv2.cvtColor(cv2.imread('blank.jpg'), cv2.COLOR_BGR2GRAY)
         #reduce to 1/4 size
         size = (int(round(0.25*template_visual.shape[1])), int(round(0.25*template_visual.shape[0])))
         template_visual = cv2.resize(template_visual, size)
-
+    
                     #-----------------------------------
                     #This is for mathematical comparison
                     #-----------------------------------
         #create criterion template, reverse size tuple
         template_math = np.zeros(size[::-1], dtype=np.uint8)
-        drawAnswerCoords(template_math)
-
+        template_math = ndrawAnswerCoords(template_math)
+        
         #adjust registered
         registered = cv2.cvtColor(registered, cv2.COLOR_BGR2GRAY)
-        retval, registered = cv2.threshold(registered, 70, 255, cv2.THRESH_BINARY)
+        retval, registered_bin = cv2.threshold(registered, 120, 255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
         #compute blend
-        blend_math = cv2.bitwise_and(registered, template_math)
+        blend_math = cv2.bitwise_and(registered_bin, template_math)
         blend_visual = cv2.addWeighted(registered, .7, template_visual, .3, 0)
 
         #impose criterion
@@ -124,26 +104,25 @@ def main():
             print blend_sum
             quota += 1
             minval = blend_sum
-        else:
-            continue
-        
-        
-        if view == "output" or view == "blend":
-            cv2.imshow("blended", blend_math)
 
+            if view == "output" or view == "blend":
+                cv2.imshow("blended", blend_visual)
+        
         '''======================================================='''
 
-        #registration criterion, only grade if we've improved 5 times
-        if quota == 4:
+        #registration criterion, only grade if we've improved 9 times
+        if quota == 6 or wk == 27:
             try:
-                grade(registered)
+                if not grade(registered):
+                    cv2.imwrite('misaligned.jpg', blend_visual)
+                break
             except:
                 if not suppress:
                     print "something bad is happening at Grade"
                 continue
-            break
         
     cv2.cv.DestroyAllWindows()
+    camera.release()
 
 if __name__ == '__main__':
     main()
